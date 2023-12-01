3ac1cb932f8ce6f7564ad81fa479c872c8d417ea
mVidarsson <marcus.vidarsson@earlybird.se>
2023-12-01 11:46:28 +0100

    Login

---
+++ src/pages/Start.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <Login />
+    </div>
+  </template>
+  
+  <script>
+  import Login from "../components/Login.vue";
+  
+  export default {
+    name: "Game",
+    components: {
+      Login,
+    },
+  };
+  </script>
+  
+  <style></style>
