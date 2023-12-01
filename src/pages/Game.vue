175489cf80a94374369103a982f932adc00105be
Olof Moriya <olof.moriya@gmail.com>
2023-12-01 13:13:41 +0100

    Adds hardcoded game to Game view

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -6,6 +6,11 @@
 
 <script>
 import Timeline from "../components/TimeLine.vue";
+import {subscribe} from "../../firebase";
+
+subscribe("games", "hast", (data)=>{
+    console.log("data in game: ", data);
+});
 
 export default {
   name: "Game",
