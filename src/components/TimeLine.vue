ce619043805405f2b60faac1400206901f0b40ef
mVidarsson <marcus.vidarsson@earlybird.se>
2023-12-01 12:58:25 +0100

    timeline

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="timeline-container">
+    <!-- Displaying the random number above the timeline -->
+    <div class="random-number-display">Random Number: {{ randomNumber }}</div>
     <div class="timeline">
-      <div
-        class="timeline-dot"
-        v-for="(dot, index) in dots"
-        :key="dot.id"
-        :class="{ 'clicked-dot': dot.clicked }"
-        @click="dotClick(index)"
-      ></div>
+      <div v-for="(item, index) in dots" :key="item.id" class="timeline-item">
+        <div v-if="item.type === 'dot'" class="timeline-dot" @click="dotClick(index)"></div>
+        <div v-else-if="item.type === 'number'" class="timeline-number">{{ item.number }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,28 +16,34 @@ export default {
   name: "Timeline",
   data() {
     return {
-      dots: [{ id: 0, clicked: false }],
+      dots: [{ id: 0, type: 'dot' }],
       nextId: 1,
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9], // Array of numbers
+      randomNumber: null
     };
+  },
+  created() {
+    this.generateRandomNumber();
   },
   methods: {
     dotClick(index) {
-      const middleIndex = Math.floor(this.dots.length / 2);
-      console.log(this.dots[index], "middleIndex");
-      this.dots[index].clicked = true;
-
-      const newDotBefore = { id: this.nextId + 1, clicked: false };
-      const newDotAfter = { id: this.nextId - 1, clicked: false };
-
-      this.dots.splice(middleIndex, 0, newDotBefore);
-      this.dots.splice(middleIndex + 1, 0, newDotAfter);
-      //}
+      if (this.randomNumber !== null) {
+        this.dots.splice(index, 1, { id: this.nextId++, type: 'number', number: this.randomNumber });
+        this.dots.splice(index, 0, { id: this.nextId++, type: 'dot' });
+        this.dots.splice(index + 2, 0, { id: this.nextId++, type: 'dot' });
+        this.generateRandomNumber(); // Generate a new random number
+      }
     },
+    generateRandomNumber() {
+      this.randomNumber = this.numbers[Math.floor(Math.random() * this.numbers.length)];
+    }
   },
 };
 </script>
 
+
 <style>
+
 .timeline-container {
   overflow-x: auto;
   white-space: nowrap;
@@ -83,6 +88,34 @@ export default {
 }
 .clicked-dot {
   background-color: #3498db;
+}
+.timeline-item {
+  display: flex;
+  align-items: center;
+}
+
+.timeline-label {
+  /* Styles for your label */
+  margin: 0 10px;
+  color: #333;
+  font-weight: bold;
+}
+.timeline-number {
+  /* Styles for your number */
+  margin: 0 10px;
+  color: #3498db;
+  font-weight: bold;
+}
+.random-number-display {
+  margin-bottom: 15px;
+  font-size: 1.2em;
+  color: #3498db;
+}
+.timeline-number {
+  /* Styles for your number */
+  margin: 0 10px;
+  color: #3498db;
+  font-weight: bold;
 }
 
 /* Rest of your styles */
