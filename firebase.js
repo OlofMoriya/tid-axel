d7956c8c238f3f39d431bc91f04d243d522ada3e
Olof Moriya <olof.moriya@gmail.com>
2023-12-01 13:04:53 +0100

    create game logic

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, getDoc, getDocs, setDoc, collection, doc } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -27,11 +27,14 @@ function subscribe(collection, game_name, update) {
     });
 }
 
-async function start_game(collection, game_name) {
+async function start_game(collection_name, game_name) {
     const querySnapshot = await getDocs(collection(db, "cards"));
     let cards = [];
     querySnapshot.forEach((doc) => {
-        cards.push(doc);
+
+        cards.push({url: doc.data().url, text: doc.data().text, year:doc.data().year});
+        console.log(cards);
+
     });
 
     function getRandomCards(cards) {
@@ -40,12 +43,13 @@ async function start_game(collection, game_name) {
     }
 
     const randomCards = getRandomCards(cards);
- 
-    return await setDoc(doc(db, collection, game_name), {
+    const obj = {
         name: game_name,
         current_card: 0,
         cards: randomCards,
-    });
+    }; 
+    console.log("setting doc", obj); 
+    return await setDoc(doc(db, "games", game_name), obj);
 }
 
 async function update_player(collection, game_name, name, cards) {
@@ -55,4 +59,4 @@ async function update_player(collection, game_name, name, cards) {
     });
 }
 
-export { db, subscribe, start_game, subscribe, update_player };
+export { db, subscribe, start_game, update_player };
