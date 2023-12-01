276e5a02c0a674f7bb4228bfbac8da0b05083ad2
mVidarsson <marcus.vidarsson@earlybird.se>
2023-12-01 14:40:00 +0100

    vue3

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="timeline-container">
-    <!-- Display the next random number above the timeline with countdown -->
-    <div v-if="randomNumber !== null" class="random-number-display">
+    <div v-if="randomNumber" class="random-number-display">
       Next Number: {{ randomNumber }} (Changing in {{ countdown }} seconds)
     </div>
     <div class="timeline">
       <div v-for="(item, index) in dots" :key="item.id" class="timeline-item">
-        <div v-if="item.type === 'dot'" :class="{ 'disabled-dot': dotUsed }" class="timeline-dot" @click="dotClick(index)"></div>
+        <div 
+          v-if="item.type === 'dot'" 
+          :class="{ 'disabled-dot': dotUsed }" 
+          class="timeline-dot" 
+          @click="() => dotClick(index)"
+        ></div>
         <div v-else-if="item.type === 'number'" class="timeline-number">{{ item.number }}</div>
       </div>
     </div>
@@ -14,77 +18,78 @@
 </template>
 
 <script>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
 export default {
   name: "Timeline",
-  data() {
-    return {
-      dots: [{ id: 0, type: 'dot' }],
-      nextId: 1,
-      numbers: this.shuffleArray([1965, 2003, 2022, 1984, 1945, 1999, 2001, 1977, 1953]),
-      randomNumber: null,
-      countdown: 5,
-      intervalId: null,
-      dotUsed: false,
-    };
-  },
-  created() {
-    this.setRandomNumberWithInterval();
-  },
-  beforeDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  },
-  methods: {
-    dotClick(index) {
-      if (!this.dotUsed && this.randomNumber !== null) {
-        const leftNumber = index > 0 && this.dots[index - 1].type === 'number' ? this.dots[index - 1].number : null;
-        const rightNumber = index < this.dots.length - 1 && this.dots[index + 1].type === 'number' ? this.dots[index + 1].number : null;
+  setup() {
+    const dots = ref([{ id: 0, type: 'dot' }]);
+    let nextId = 1;
+    const numbers = ref([1965, 2003, 2022, 1984, 1945, 1999, 2001, 1977, 1953].sort(() => 0.5 - Math.random()));
+    const randomNumber = ref(null);
+    const countdown = ref(5);
+    const intervalId = ref(null);
+    const dotUsed = ref(false);
 
-        if (this.isValidNumber(this.randomNumber, leftNumber, rightNumber)) {
-          this.dots.splice(index, 1, { id: this.nextId++, type: 'number', number: this.randomNumber });
-          this.dots.splice(index, 0, { id: this.nextId++, type: 'dot' });
-          this.dots.splice(index + 2, 0, { id: this.nextId++, type: 'dot' });
-        } else {
-          console.log('Random number does not fit between adjacent numbers.');
-        }
-        this.dotUsed = true; // Disable further clicks, regardless of whether the click was right or wrong
-      }
-    },
-    setRandomNumberWithInterval() {
-      if (this.intervalId) {
-        clearInterval(this.intervalId);
-      }
-      this.updateRandomNumber();
-      this.intervalId = setInterval(() => {
-        if (this.countdown > 0) {
-          this.countdown -= 1;
-        } else {
-          this.updateRandomNumber();
-          this.countdown = 5;
-        }
-      }, 1000);
-    },
-    updateRandomNumber() {
-      if (this.numbers.length > 0) {
-        this.randomNumber = this.numbers.pop();
-        this.dotUsed = false; // Allow dot clicks for the new number
-      } else {
-        this.randomNumber = null;
-        clearInterval(this.intervalId);
-      }
-    },
-    shuffleArray(array) {
+    function shuffleArray(array) {
       for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
       }
       return array;
-    },
-    isValidNumber(number, leftNumber, rightNumber) {
+    }
+
+    function isValidNumber(number, leftNumber, rightNumber) {
       return (!leftNumber || number >= leftNumber) && (!rightNumber || number <= rightNumber);
     }
-  },
+
+    function dotClick(index) {
+      if (!dotUsed.value && randomNumber.value) {
+        const leftNumber = index > 0 && dots.value[index - 1]?.type === 'number' ? dots.value[index - 1].number : null;
+        const rightNumber = index < dots.value.length - 1 && dots.value[index + 1]?.type === 'number' ? dots.value[index + 1].number : null;
+
+        if (isValidNumber(randomNumber.value, leftNumber, rightNumber)) {
+          dots.value.splice(index, 1, { id: nextId++, type: 'number', number: randomNumber.value });
+          dots.value.splice(index, 0, { id: nextId++, type: 'dot' });
+          dots.value.splice(index + 2, 0, { id: nextId++, type: 'dot' });
+        }
+        dotUsed.value = true;
+      }
+    }
+
+    function updateRandomNumber() {
+      if (numbers.value.length > 0) {
+        randomNumber.value = numbers.value.pop();
+        dotUsed.value = false; // Reset for the new number
+      } else {
+        randomNumber.value = null;
+        if (intervalId.value) {
+          clearInterval(intervalId.value);
+        }
+      }
+    }
+
+    function setRandomNumberWithInterval() {
+      updateRandomNumber();
+      intervalId.value = setInterval(() => {
+        if (countdown.value > 0) {
+          countdown.value -= 1;
+        } else {
+          updateRandomNumber();
+          countdown.value = 5;
+        }
+      }, 1000);
+    }
+
+    onMounted(setRandomNumberWithInterval);
+    onBeforeUnmount(() => {
+      if (intervalId.value) {
+        clearInterval(intervalId.value);
+      }
+    });
+
+    return { dots, randomNumber, countdown, dotClick, dotUsed };
+  }
 };
 </script>
 
@@ -123,12 +128,10 @@ export default {
   }
 
   .timeline-number {
-    /* Your styles for the timeline number */
+    /* Styles for the timeline number */
   }
 
   .random-number-display {
-    /* Your styles for the random number display */
+    /* Styles for the random number display */
   }
-
-  /* Add any additional styles you need here */
 </style>
