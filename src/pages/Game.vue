25f0a769323ed3e778f6e2496304de0bcbdd65b5
igrenholm <ingrid.grenholm@earlybird.se>
2023-12-03 19:07:16 +0100

    styling

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <Timeline :currentCard="currentCard" :id="id"/>
+    <Timeline :currentCard="currentCard" :id="id" />
   </div>
 </template>
 
 <script>
-import { ref, onMounted, watchEffect} from "vue";
+import { ref, onMounted, watchEffect } from "vue";
 import Timeline from "../components/TimeLine.vue";
 import { next_card, subscribe } from "../../firebase";
 import { useRoute } from "vue-router";
@@ -16,39 +16,38 @@ export default {
     Timeline,
   },
 
-    setup() {
-        const route = useRoute();
-        const currentCard = ref(null);
-        const game = ref(null);
-        watchEffect(() => console.log("WHat's happening here?", currentCard.value?.text));
+  setup() {
+    const route = useRoute();
+    const currentCard = ref(null);
+    const game = ref(null);
+    watchEffect(() =>
+      console.log("WHat's happening here?", currentCard.value?.text)
+    );
 
-        const id = route.params.id;
+    const id = route.params.id;
 
-        onMounted(async ()=>
-            {
-                subscribe("games", id, (data) => {
-                    game.value = data;
-                    currentCard.value = data.cards[data.current_card];
-                })
+    onMounted(async () => {
+      subscribe("games", id, (data) => {
+        game.value = data;
+        currentCard.value = data.cards[data.current_card];
+      });
 
-                const interval = setInterval(()=>{
-                    next_card("games", id, game.value, game.value?.current_card +1); 
-                    if (game.value?.current_card == game.value?.cards.length - 2){
-                        console.log("end of the line");
-                        clearInterval(interval);
-                    }
-                }, 15000);
-            }
+      const interval = setInterval(() => {
+        next_card("games", id, game.value, game.value?.current_card + 1);
+        if (game.value?.current_card == game.value?.cards.length - 2) {
+          console.log("end of the line");
+          clearInterval(interval);
+        }
+      }, 10000);
+    });
 
-        );
+    console.log("game", id);
 
-        console.log("game", id);
-
-        return {
-            currentCard,
-            id,
-        };
-    },
+    return {
+      currentCard,
+      id,
+    };
+  },
 };
 </script>
 
